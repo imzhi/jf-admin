31fbc165ce4a70fac13d084872a089795b41c932
imzhi <yxz_blue@126.com>
2019-12-05 17:08:48 +0800

    permission_extras 表迁移文件根据 spatie/laravel-permission 扩展包的版本来决定外键是 int 还是 bigint 类型

--- database/migrations/2019_07_04_141217_create_permission_extras_table.php
+++ database/migrations/2019_07_04_141217_create_permission_extras_table.php
@@ -15,19 +15,27 @@ class CreatePermissionExtrasTable extends Migration
     {
         $tableNames = config('permission.table_names');
 
+        Schema::disableForeignKeyConstraints();
+
         Schema::create('permission_extras', function (Blueprint $table) use ($tableNames) {
-            $table->unsignedInteger('permission_id');
+            if ($this->laravelPermissionVersion()) {
+                $table->unsignedInteger('permission_id');
+            } else {
+                $table->unsignedBigInteger('permission_id');
+            }
 
             $table->string('extra_cate');
             $table->string('extra_name');
 
-            // $table->foreign('permission_id')
-            //     ->references('id')
-            //     ->on($tableNames['permissions'])
-            //     ->onDelete('cascade');
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on($tableNames['permissions'])
+                ->onDelete('cascade');
 
             $table->primary('permission_id');
         });
+
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
@@ -38,5 +46,20 @@ class CreatePermissionExtrasTable extends Migration
     public function down()
     {
         Schema::dropIfExists('permission_extras');
+    }
+
+    /**
+     * 检查 spatie/laravel-permission 扩展包版本是否小于 3.1.0
+     * （大于等于 3.1.0 时 permissions 表 ID 字段是 bigint 类型）
+     *
+     * @return bool
+     */
+    protected function laravelPermissionVersion()
+    {
+        $installed = json_decode(file_get_contents(base_path().'/vendor/composer/installed.json'), true);
+
+        $installed_name = collect($installed)->keyBy('name');
+
+        return version_compare($installed_name['spatie/laravel-permission']['version'], '3.1.0', '<');
     }
 }
